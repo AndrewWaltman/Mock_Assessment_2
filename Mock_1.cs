60b643126fc78491f98c1810b488d6147d42d555
Andrew Waltman <Andrew.W.Waltman@gmail.com>
2020-02-17 17:38:42 -0500

    Setting up the mock

---
+++ Mock_1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mock_Assessment_2
+{
+    class Mock_1
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mock_Assessment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
