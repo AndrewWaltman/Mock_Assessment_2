6c9670680348cd1e763c90adb5130e63a51b6137
Andrew Waltman <Andrew.W.Waltman@gmail.com>
2020-02-19 17:28:21 -0500

    Now with Comments!

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace Mock_Assessment_2
             tasks.Add("Wake up");
             tasks.Add("Brush your vegetables");
             tasks.Add("Eat your teeth");
+            //List of the things.
 
             Console.WriteLine("Here are your tasks for today.");
             Console.WriteLine("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
@@ -19,18 +20,23 @@ namespace Mock_Assessment_2
             {
                 Console.WriteLine($"{i}: {tasks[i]}");
             }
+            //Displays the list to see.
 
             Console.WriteLine("\nWhat would you like to do to the list?");
             Console.WriteLine("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+==+=+=+==+=+=+=");
             Console.WriteLine("1. Add a task to the list.\n2. Finish a task.\n3. Grab a task.");
             string input = Console.ReadLine();
+            //Asks the first question.
+            //Each if else statements that follow are for the above response. If not, end program.
 
             if (input == "1")
             {
                 Console.WriteLine("Please write out what needs to be done.");
                 string addList = Console.ReadLine();
+                //Make sure the variable grabbed matches the following Method parameter.
 
                 AddTask(addList);
+                //Asks for string parameter.
             }
             else if (input == "2")
             {
@@ -43,17 +49,21 @@ namespace Mock_Assessment_2
             {
                 Console.WriteLine("Which task would you like? (Numbers only please)");
                 int grabTask = int.Parse(Console.ReadLine());
+                
                 GetTask(grabTask);
+                //Asks for an int, must int.Parse();
             }
             else
             {
                 Console.WriteLine("WRONG! GOODBYE!");
+                //else catch statement
             }
 
         }
         public static void AddTask(string a)
         {
             tasks.Add(a);
+            //Takes in the string from before (input) and adds it to the list.
 
             Console.WriteLine("Here is your new list.");
             Console.WriteLine("=+=+=+=+=+=+=+=+=+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=");
@@ -61,19 +71,23 @@ namespace Mock_Assessment_2
             {
                 Console.WriteLine($"{i}: {tasks[i]}");
             }
+            //Displays the new list and closes.
         }
         public static bool FinishTask(string f)
         {
             for (int i = 0; i < tasks.Count; i++)
+            //Same loop.
             {
+                //Uses the Contains method to match if the new string is available in the tasks list
                 if (tasks.Contains(f))
                 {
                     Console.WriteLine("Okay, we will check that off of the list.");
-                    Console.WriteLine($"{f} was completed: {true}.\n");
+                    Console.WriteLine($"{f} was completed: {true}.\n"); //Returns true.
                     Console.WriteLine("=+=+=+=+=+=+=+=+=+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=");
-                    tasks.Remove(f);
-                    break;
+                    tasks.Remove(f);//Removes the task when it is a match.
+                    break;//Break just for safety.
                 }
+                //Otherwise returns false and does nothing.
                 else if(!tasks.Contains(f))
                 {
                     Console.WriteLine("Sorry, that is not on the list.");
@@ -82,15 +96,15 @@ namespace Mock_Assessment_2
                     break;
                 }
             }
-            for (int i = 0; i < tasks.Count; i++)
+            for (int i = 0; i < tasks.Count; i++) //Reposts the new list after the removal.
             {
                 Console.WriteLine($"{i}: {tasks[i]}");
             }
-            return true;
+            return true; //Must return a bool because it is not a void method.
         }
         public static string GetTask(int g)
         {
-            try
+            try //Using try catch methods.
             {
                 _ = tasks[g] == tasks[0];
 
@@ -98,7 +112,7 @@ namespace Mock_Assessment_2
                 Console.WriteLine("Here is your list item.");
                 Console.WriteLine($"{tasks[g]}");
             }
-            catch (ArgumentOutOfRangeException e)
+            catch (IndexOutOfRangeException e)
             {
                 Console.WriteLine("Error", e);
             }
